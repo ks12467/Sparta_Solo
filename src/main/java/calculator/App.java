a123f40192021b610040821962893b0602560b93
Robbie <robbie331@>
2024-07-25 17:00:57 +0900

    Leve 1-1 요구사

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        /*Scanner를 사용하여 입력 값을 받을 수 있음 */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하시오. : ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하시오. : ");
+        int num2 = sc.nextInt();
+
     }
 }
