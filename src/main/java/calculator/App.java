e85f0457564bd0b6b69fd92cb96565911235079b
Robbie <robbie331@>
2024-07-25 19:28:54 +0900

    Leve 1-3 요구사항

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,6 +8,7 @@ public class App {
         /*Scanner를 사용하여 입력 값을 받을 수 있음 */
         Scanner sc = new Scanner(System.in);
 
+
         System.out.print("첫 번째 숫자를 입력하시오. : ");
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하시오. : ");
@@ -15,7 +16,33 @@ public class App {
 
         System.out.print("연산 기호를 입력하시오. : ");
         char operator = sc.next().charAt(0);
-        
+
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("다른 값을 입력해주세요");
+                    return;
+
+                }
+                break;
+            default:
+                System.out.println("연산자가 바르지 않습니다.");
+                return;
+        }
+        System.out.println("결과 : " + result);
 
     }
 }
