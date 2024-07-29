193f0756c8b87023a37667c3224977bb957c4276
Robbie <robbie331@>
2024-07-29 10:20:46 +0900

    level 2-1 요구사하우ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,30 @@
 package calculator;
 
 public class Calculator {
+    public int Calclator(int num1, int num2, char operator ) throws badExceptions {
+    int result;
 
-}
+            switch(operator){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                throw new badExceptions("입력값이 올바르지 않습니다.");
+            }
+            break;
+        default:
+            throw new badExceptions("연산자 입력이 올바르지 않습니다.");
+        }
+            return result;
+            }
+    }
+
---
+++ src/main/java/calculator/badExceptions.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class badExceptions extends Exception {
+    public badExceptions(String message) {
+        super(message);
+    }
+
+}
