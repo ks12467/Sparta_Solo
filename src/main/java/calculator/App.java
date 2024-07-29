8d9491c0e8db02cd854fda05f80cfefb88ea7a68
Robbie <robbie331@>
2024-07-29 11:05:45 +0900

    level 2-2 요구사하우ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,81 +6,65 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        /*Scanner를 사용하여 입력 값을 받을 수 있음 */
+        ArrayList<Integer> results = new ArrayList<>();/*Scanner를 사용하여 입력 값을 받을 수 있음 */
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> results = new ArrayList<>();
+        Calculator calc = new Calculator();
+
 
 
         while (true) {
-            /* 기록확인 및 계산을 더 할 것인지를 추가함 */
-            System.out.println("계산 진행 y / 기록 확인 history");
-            /* 문자를 입력받기 위한 메서드 */
-            String str = sc.next();
-            /* equeals = 객체간의 값을 비교 */
-            if (str.equals("history")) {
-                /* 만약에 results에 저장된 값이 없으면 아래 문장 출력*/
-                if (results.isEmpty()) {
-                    System.out.println("기록이 없습니다.");
-                    /*기록이 없으면 처음으로 돌아감 */
-                    continue;
-                } else {
-                    System.out.println("저장된 기록 확인");
-                    /* 향상된 for문을 사용해서 저장된 기록을 출력*/
-                    int index = 1;
-                    for (int result : results){
-                        System.out.println("[" +index + "] : " + result);
-                        index++;
+            try {
+                /* 기록확인 및 계산을 더 할 것인지를 추가함 */
+                System.out.println("계산 진행 y / 기록 확인 history");
+                /* 문자를 입력받기 위한 메서드 */
+                String str = sc.next();
+                /* equeals = 객체간의 값을 비교 */
+                if (str.equals("history")) {
+                    /* 만약에 results에 저장된 값이 없으면 아래 문장 출력*/
+                    if (results.isEmpty()) {
+                        System.out.println("기록이 없습니다.");
+                        /*기록이 없으면 처음으로 돌아감 */
+                        continue;
+                    } else {
+                        System.out.println("저장된 기록 확인");
+                        /* 향상된 for문을 사용해서 저장된 기록을 출력*/
+                        int index = 1;
+                        for (int result : results) {
+                            System.out.println("[" + index + "] : " + result);
+                            index++;
+                        }
                     }
+                } else if (str.equals("y")) {
+                    System.out.println("계산을 시작합니다.");
+                } else {
+                    System.out.println("입력 값이 올바르지 않습니다.");
+                    continue;
                 }
-            } else if (str.equals("y")) {
-                System.out.println("계산을 시작합니다.");
-            } else {
-                System.out.println("입력 값이 올바르지 않습니다.");
+                System.out.print("첫 번째 숫자를 입력하시오. : ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하시오. : ");
+                int num2 = sc.nextInt();
+
+                System.out.print("연산 기호를 입력하시오. : ");
+                char operator = sc.next().charAt(0);
+
+                int result = calc.Calculate(num1, num2, operator);
+            } catch (badExceptions e) {
+                System.out.println("올바르지 않습니다. ");
+                continue;
+            } catch (Exception e) {
+                System.out.println("올바르지 없습니다. ");
                 continue;
             }
-            System.out.print("첫 번째 숫자를 입력하시오. : ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하시오. : ");
-            int num2 = sc.nextInt();
 
-            System.out.print("연산 기호를 입력하시오. : ");
-            char operator = sc.next().charAt(0);
-
-            int result;
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("다른 값을 입력해주세요");
-                        continue;
-                    }
-                    break;
-                default:
-                    System.out.println("연산자가 바르지 않습니다.");
-                    continue;
-            }
-            System.out.println("결과 : " + result);
-            results.add(result);
-
+            /*----------------------------------------------------*/
+            /* 결과 값 삭제 및 ArrayList에 저장*/
             /*결과 값을 ArrayList에 저장*/
             for (int i = 0; i < results.size(); i++) {
                 System.out.println((i + 1) + " : " + results.get(i));
             }
-
             System.out.println("계산을 더 하신거나 결과를 삭제하시겠습니까? (y/n/remove)");
-            str = sc.next();
-
+            String str = sc.next();
             if (str.equals("y")) {
                 System.out.println("계산을 다시 시작합니다.");
             } else if (str.equals("remove")) {
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,30 +1,37 @@
 package calculator;
 
-public class Calculator {
-    public int Calclator(int num1, int num2, char operator ) throws badExceptions {
-    int result;
+import java.util.ArrayList;
 
-            switch(operator){
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                throw new badExceptions("입력값이 올바르지 않습니다.");
-            }
-            break;
-        default:
-            throw new badExceptions("연산자 입력이 올바르지 않습니다.");
+public class Calculator {
+    public int Calculate(int num1, int num2, char operator) throws badExceptions {
+        ArrayList<Integer> results = new ArrayList<>();
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    throw new badExceptions("입력값이 올바르지 않습니다.");
+                }
+                break;
+            default:
+                throw new badExceptions("연산자 입력이 올바르지 않습니다.");
         }
-            return result;
-            }
+
+        System.out.println("결과 : " + result);
+        results.add(result);
+
+        return result;
     }
+}
 
